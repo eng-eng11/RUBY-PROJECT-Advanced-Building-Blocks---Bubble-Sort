63903ae4e14f029b17a71a7fed0ed33861d6744d
falguniislam <islamfalguni12@gmail.com>
2019-10-14 21:02:40 +0200

    tse correction 02

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 # rubocop:disable Style/WordArray
 def bubble_sort_by(array)
   not_sorted = true, n = array.length - 1
   while not_sorted
     not_sorted = false
     (0...n).each do |i|
-      next unless yield(array[i], array[i + 1]) > 0
-
+      yield(array[i], array[i + 1]).positive?
       array[i], array[i + 1] = array[i + 1], array[i], not_sorted = true
     end
     n -= 1
