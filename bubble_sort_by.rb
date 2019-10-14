5b155d0d27e45f054670298022bd67e5d0829bf5
falguniislam <islamfalguni12@gmail.com>
2019-10-14 16:43:21 +0200

    bubble sort by added

---
+++ bubble_sort_by.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/WordArray
+def bubble_sort_by(array)
+  not_sorted = true, n = array.length - 1
+  while not_sorted
+    not_sorted = false
+    (0...n).each do |i|
+      yield(array[i], array[i + 1]).positive?
+
+      array[i], array[i + 1] = array[i + 1], array[i], not_sorted = true
+    end
+    n -= 1
+  end
+  array
+end
+
+array = ['hi', 'hello', 'hey']
+bubble_sort_by(array) do |left, right|
+  left.length <=> right.length
+end
+p array
+
+#=> ["hi", "hey", "hello"]
+
+# rubocop:enable Style/WordArray
