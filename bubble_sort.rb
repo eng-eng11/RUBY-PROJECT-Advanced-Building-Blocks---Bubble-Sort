b29096d2db487210c3f3880395efd54549e5580b
falguniislam <islamfalguni12@gmail.com>
2019-10-17 23:09:04 +0200

    tse correction 07 done

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,7 +13,7 @@ def bubble_sort(arr)
       i += 1
       swap += 1
     end
-    break if swap == 0
+    break if swap.zero?
   end
   arr
 end
@@ -37,7 +37,7 @@ def bubble_sort_by(arr)
       i += 1
       swap += 1
     end
-    break if swap == 0
+    break if swap.zero?
   end
   arr
 end
