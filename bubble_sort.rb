6e09935713dc06f7967605eab10d3de5da8b90ff
falguniislam <islamfalguni12@gmail.com>
2019-10-18 14:21:30 +0200

    tse new feature correcction

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,45 +1,37 @@
 # frozen_string_literal: true
 
-def bubble_sort(arr)
-  arr.each do
-    i = 0
-    swap = 0
-    while i < arr.length - 1
-      if arr[i] > arr[i + 1]
-        temp = arr[i]
-        arr[i] = arr[i + 1]
-        arr[i + 1] = temp
-      end
-      i += 1
-      swap += 1
+def bubble_sort(array)
+  (0..array.size - 2).each do |_i|
+    (0..array.size - 2).each do |j|
+      next unless array[j] <=> (array[j + 1]).positive?
+
+      tmp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = tmp
     end
-    break if swap.zero?
   end
-  arr
+
+  array
 end
 
 p bubble_sort([3, 1, 5, 6, 2, 4, 7, 8])
 
-def bubble_sort_by(arr)
-  arr.each do |_left, _right|
-    i = 0
-    swap = 0
+def bubble_sort_by(array)
+  return bubble_sort(array) unless block_given?
 
-    while i < arr.length - 1
-      yield_ = yield(arr[i], arr[i + 1]) if block_given?
+  (0..array.size - 2).each do |_i|
+    (0..array.size - 2).each do |j|
+      cmp = yield(array[j], array[j + 1])
 
-      yield_ = arr[i] > arr[i + 1]
-      if yield_
-        temp = arr[i]
-        arr[i] = arr[i + 1]
-        arr[i + 1] = temp
-      end
-      i += 1
-      swap += 1
+      next unless cmp.positive?
+
+      tmp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = tmp
     end
-    break if swap.zero?
   end
-  arr
+
+  array
 end
 
 p bubble_sort_by(%w[hill hel world]) { |left, right| left.length - right.length }
