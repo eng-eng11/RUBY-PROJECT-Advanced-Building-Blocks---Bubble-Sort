6b200ae657baa6f9cb5888f694092a26dd9ab254
davidauza.engineer <davidauza01@gmail.com>
2019-10-22 10:27:30 -0500

    Add remaining elements to algorithm

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,38 +1,37 @@
 # frozen_string_literal: true
 
-def bubble_sort(array)
-  (0..array.size - 2).each do |_i|
-    (0..array.size - 2).each do |j|
-      next unless array[j] <=> (array[j + 1]).positive?
-
-      tmp = array[j]
-      array[j] = array[j + 1]
-      array[j + 1] = tmp
+# this method sorts a given array of numbers using a bubble sort algorithm
+def bubble_sort(arr)
+  arr.each do
+    i = 0
+    swap = 0
+    while i < arr.length - 1
+      if arr[i] > arr[i + 1]
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+      end
+      i += 1
+      swap += 1
     end
+    break if swap.zero?
   end
-
-  array
 end
 
-p bubble_sort([3, 1, 5, 6, 2, 4, 7, 8])
-
+# this method sorts an array by accepting a block
 def bubble_sort_by(array)
-  raise 'No block_given' if block_given?
-  return bubble_sort(array) unless block_given?
+  raise 'No block_given' unless block_given?
 
-  (0..array.size - 2).each do |_i|
-    (0..array.size - 2).each do |j|
-      cmp = yield(array[j], array[j + 1])
+  len = array.length - 1
+  len.step(1, -1) do |a|
+    (0...a).each do |i|
+      test = yield(array[i], array[i + 1])
+      next unless test.positive?
 
-      next unless cmp.positive?
-
-      tmp = array[j]
-      array[j] = array[j + 1]
-      array[j + 1] = tmp
+      temp = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = temp
     end
   end
-
-  array
+  puts array
 end
-
-p bubble_sort_by(%w[hill hel world]) { |left, right| left.length - right.length }
